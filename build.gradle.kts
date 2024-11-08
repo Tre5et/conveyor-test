f5a3a796910859310247d8b572a45e93d36b65fb
TreSet <hanneshekl@gmail.com>
2024-11-08 17:58:45 +0100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+    id("dev.hydraulic.conveyor") version "1.10"
+}
+
+group = "net.treset"
+version = "2.0"
+
+repositories {
+    mavenCentral()
+    google()
+    maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+kotlin {
+    jvm {
+        withJava()
+    }
+    jvmToolchain(21)
+
+    sourceSets {
+        val jvmMain: KotlinSourceSet by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+                implementation(compose.desktop.currentOs)
+
+                // Conveyor API: Manage automatic updates.
+                implementation("dev.hydraulic.conveyor:conveyor-control:1.1")
+            }
+        }
+    }
+}
+
+dependencies {
+    // Use the configurations created by the Conveyor plugin to tell Gradle/Conveyor where to find the artifacts for each platform.
+    linuxAmd64(compose.desktop.linux_x64)
+    macAmd64(compose.desktop.macos_x64)
+    macAarch64(compose.desktop.macos_arm64)
+    windowsAmd64(compose.desktop.windows_x64)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
+}
+
+// region Work around temporary Compose bugs.
+configurations.all {
+    attributes {
+        // https://github.com/JetBrains/compose-jb/issues/1404#issuecomment-1146894731
+        attribute(Attribute.of("ui", String::class.java), "awt")
+    }
+}
+// endregion
